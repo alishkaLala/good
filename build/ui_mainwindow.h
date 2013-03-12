5787a021c51ac69ea1ddd77bddc5fe0e7b1760e7
alishkaLala <aly_6@ukr.net>
2013-03-12 02:00:52 +0200

    default

--- build/ui_mainwindow.h
+++ build/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Tue 12. Mar 01:13:18 2013
+** Created: Tue 12. Mar 01:58:56 2013
 **      by: Qt User Interface Compiler version 4.7.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
